59f1246a0d8df7fd09f8ee4437b8aeab4e8db347
progamadorvinicius <vinicius.brow244@gmail.com>
2026-05-14 11:37:36 +0100

    teste e finalização

--- script.js
+++ script.js
@@ -2,8 +2,37 @@ const display = document.getElementById("display");
 const mood = document.getElementById("mood");
 
 
-const moods = {
+const moods = [
     "Math is FUN!"
-    "Let's Good!", 
-    
+    "Let's Gooo!", 
+    "Number love you",
+    "Big brain moment",
+    "Calculated like a pro"
+]
+
+
+function appendValue(value){
+    if (display.innerText ==="0") {
+        display.innerText = value;
+    }
+    else{
+        display.innerText += value;
+    }
+
+    mood.innerText = moods[Math.floor(Math.random() + moods.length)];
+}
+
+function clearDisplay(){
+    display.innerText = "0";
+    mood.innerText = "Fresh Start"
+}
+
+function calculate (){
+    try{
+        display.innerText = eval(display.innerText);
+        mood.innerText = "BOOM ! Result Achived"
+    } catch {
+        display.innerText = "Error";
+        mood.innerText ="OOps ! try again"
+    }
 }
